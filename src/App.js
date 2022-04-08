679b5cbf94b1a5c3e6cad06f37789f86397505a2
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-08 22:23:26 +0600

    added header nav and styled them

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Header from './Components/Header/Header';
 
 function App() {
-  return <div className="App">{'app'}</div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
