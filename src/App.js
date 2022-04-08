58415e0de8b850814479a1f4b17264cd4c8d6fb5
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-09 01:33:17 +0600

    added protected route for order page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
+import Orders from './Components/Orders/Orders';
 import Products from './Components/Products/Products';
 import Register from './Components/Register/Register';
+import RequireAuth from './Components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -14,6 +18,18 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
         <Route path="/products" element={<Products></Products>}></Route>
+
+        <Route
+          path="/orders"
+          element={
+            <RequireAuth>
+              <Orders></Orders>
+            </RequireAuth>
+          }
+        ></Route>
+
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
     </div>
