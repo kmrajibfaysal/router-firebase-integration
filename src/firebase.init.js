82a4d11345d2f16857de90bfd7e88e5d54212898
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-08 21:14:54 +0600

    added dependencies installed firebase and react router and made skeliton

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA0vEj8NbRInh1w5VluRMDwp78WhAoo-LA',
+  authDomain: 'router-firebase-integrat-79f72.firebaseapp.com',
+  projectId: 'router-firebase-integrat-79f72',
+  storageBucket: 'router-firebase-integrat-79f72.appspot.com',
+  messagingSenderId: '531872704347',
+  appId: '1:531872704347:web:091e4ff1166a72f2059c7e',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
