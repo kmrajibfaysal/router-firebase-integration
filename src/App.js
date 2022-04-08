22c68d47fd7d8036084680f2adf9399bc3dd2ae9
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-09 00:16:56 +0600

    added more routes and used firebase custom hook

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
+import Products from './Components/Products/Products';
 import Register from './Components/Register/Register';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/products" element={<Products></Products>}></Route>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
     </div>
--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import useFirebase from '../../hooks/useFirebase';
 
-const Home = () => {
+const Products = () => {
   const { user } = useFirebase();
   return (
     <div>
-      <h2>This is Home!</h2>
-      <p>Current user is: {user ? user.displayName : 'No body!'}</p>
+      <h2>Who is there?</h2>
+      <h5>{user ? user.displayName : 'No body'}</h5>
     </div>
   );
 };
 
-export default Home;
+export default Products;
